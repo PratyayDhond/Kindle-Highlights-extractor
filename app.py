3e7bfe7dd2f5d6f266e86a36a9cee0fdc7006be9
Pratyay Dhond <dhondpratyay@gmail.como>
2023-04-06 16:47:18 +0530

    Added zipping option for the generated highlights

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from scraper import *
 from werkzeug.utils import secure_filename
 import os
+import platform
 
 app = Flask(__name__)
 
@@ -22,21 +23,23 @@ def fileSubmitted():
     bookData = scrapeData('Uploads/MyClippings.txt')
 
     for book in bookData:
+        title = book.getTitle()
         with open('tempFile.md','w') as md:
-           md.write(f"## {book.getTitle()}\n")
-           md.write(f"by _{book.getAuthor()}_\n")
+#           md.write(f"<h2>{title}</h2>\n")
+           md.write(f"<div style='font-size:36px; text-align:center;'><b>{title}</b></div>\n")
+#           md.write(f"<p style='position:absolute; right:0; padding-right:20px;'>by _{book.getAuthor()}_<p>\n")
+           md.write(f"<p style='text-align:end;'> \- by _{book.getAuthor()}_<p>\n")
            md.write("____\n")
            md.write("<br>\n")
            for quote in book.getQuotes():
-#               md.write(f"\" {quote['quote']} \"<br>\n")
-               md.write(f"\> {quote['quote']} <br>\n")
-               md.write(f"<font size=\"0.1\">{quote['locationPrefix']} _{quote['location']}_ | {quote['timestamp']} </font><br><br>")
-           # md.write(f"<h2 style='text-align:centre'> {book.getTitle()} </h2><br>\n")
-           # md.write(f"<h4 style='text-align:right; padding-left:20vw'> - {book.getAuthor()} </h4><br>")
+               md.write(f"“{quote['quote']}”\n<br>")
+               md.write(f"<span style='font-size:12px'>{quote['locationPrefix']} _{quote['location']}_ | {quote['timestamp']} </span><br><br>")
+        if os.path.exists('highlights') == False:
+           os.system('mkdir highlights')
+        os.system(f'md2pdf --o highlights/"{title}.pdf" tempFile.md')
 
-        if os.path.exists('outputPdfs') == False:
-            os.system('mkdir outputPdfs')
-        
-        os.system(f'md2pdf --o outputPdfs/"{book.getTitle()}.pdf" tempFile.md')
-
+    if platform.platform() == 'Windows':
+        os.system(f'zip Highlights.zip highlights')
+    else:
+        os.system(f'tar -cf Highlights.tar highlights')    
     return render_template('fileSubmitted.html',file=f.filename)
